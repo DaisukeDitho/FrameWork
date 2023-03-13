430f41a5e38ba84295b0705b06141e478f338d28
DaisukeDitho <120396130+DaisukeDitho@users.noreply.github.com>
2023-03-13 10:52:56 +0100

    sprint2

--- FrontServlet.java
+++ FrontServlet.java
@@ -1,10 +1,13 @@
 package etu1971.framework.servlet;
 
+import etu1971.framework.Mapping;
+import java.util.HashMap;
 import java.io.*;
 import jakarta.servlet.*;
 import jakarta.servlet.http.*;
 
 public class FrontServlet extends HttpServlet {
+    HashMap<String,Mapping> mappingUrls;
 
     public void processRequest(HttpServletRequest request, HttpServletResponse response)throws ServletException, IOException 
     {
---
+++ Mapping.java
@@ -0,0 +1,13 @@
+package etu1971.framework;
+
+public class Mapping 
+{
+    String className;
+    String method;
+
+    public Mapping(String className,String method)
+    {
+        this.className=className;
+        this.method=method;
+    }
+}
