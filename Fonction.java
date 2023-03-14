2c7b1fd32ba2a7082f65af005fbcc745097ee2ea
DaisukeDitho <120396130+DaisukeDitho@users.noreply.github.com>
2023-03-14 11:50:15 +0100

    sprint3

---
+++ Fonction.java
@@ -0,0 +1,76 @@
+package fonction;
+
+import java.lang.annotation.*;
+import java.util.Vector;
+import java.io.File;
+import java.lang.reflect.*;
+import monAnnotation.*;
+import java.util.HashMap;
+import etu1971.framework.*;
+
+public class Fonction{
+    
+    public static Vector<Class<?>> findClasses(String packageName)
+    {
+        Vector<Class<?>> classes = new Vector<Class<?>>();
+        String[]yu=packageName.split("/");
+
+        //String basePath = packageName.replace(".", "/");
+        File directory = new File(packageName);
+        if (!directory.exists()) {
+            return classes;
+        }
+        File[] files = directory.listFiles();
+        for (File file : files) {
+            String fileName = file.getName();
+            if (file.isFile() && fileName.endsWith(".class")) {
+                packageName=yu[yu.length-1];
+                String className = packageName + "." + fileName.substring(0, fileName.length() - 6);
+                //System.out.println("vita : "+className);
+                try {
+                    //System.out.println("voalohany : "+fileName);
+
+                    Class<?> clazz = Class.forName(className);
+                    classes.add(clazz);
+                } catch (ClassNotFoundException e) {
+                    e.printStackTrace();
+                    // Handle exception
+                }
+            } else if (file.isDirectory()) {
+                Vector<Class<?>> subClasses = findClasses(packageName + "." + fileName);
+                classes.addAll(subClasses);
+            }
+        }
+        return classes;
+    }
+
+    public static HashMap<String,Mapping> findAnnotatedMethod( String packageName) 
+    {
+        HashMap<String,Mapping> list = new HashMap<String,Mapping>();
+        String[]result=new String[3];
+        Vector<Class<?>> annotatedClasses = new Vector<Class<?>>();
+        Vector<Class<?>> classes = findClasses(packageName);
+        for (Class<?> clazz : classes) {
+            Method[]allMeth=clazz.getDeclaredMethods();
+            for (Method one : allMeth) {
+                Annotation[]annot=one.getDeclaredAnnotations();
+                for (Annotation aa : annot) {
+                    Model anah=(Model)aa;
+                    
+                        result[0]=clazz.getName();
+                        result[1]=one.getName();
+                        result[2]=anah.url();
+                        System.out.println("Classe : "+result[0]);
+                        System.out.println("Fonction : "+result[1]);
+                        System.out.println("URL : "+result[2]);
+                        System.out.println("--------------------------------");
+
+                        Mapping koko=new Mapping(result[0],result[1]);
+                        list.put(result[2],koko);
+                        System.out.println("isany : "+list.size());
+                }
+            }
+        }
+        return list;
+    }
+}   
--- FrontServlet.java
+++ FrontServlet.java
@@ -3,18 +3,37 @@ package etu1971.framework.servlet;
 import etu1971.framework.Mapping;
 import java.util.HashMap;
 import java.io.*;
+import java.nio.file.*;
 import jakarta.servlet.*;
 import jakarta.servlet.http.*;
+import fonction.*;
+import model.*;
 
 public class FrontServlet extends HttpServlet {
     HashMap<String,Mapping> mappingUrls;
+
+    public void setmappingUrls(HashMap<String,Mapping> haha)
+    {
+        this.mappingUrls=haha;
+    }
+    public HashMap<String,Mapping> getmappingUrls()
+    {
+        return this.mappingUrls;
+    }
+
+    public void init()
+    {
+        String popo=getServletContext().getRealPath("/WEB-INF/classes");
+        Path lololol=Paths.get(popo+"/model");
+        setmappingUrls(Fonction.findAnnotatedMethod("C:/Program Files/Apache Software Foundation/Tomcat 10.0/webapps/Construct-FrameWork/WEB-INF/classes/model")); 
+    }
 
     public void processRequest(HttpServletRequest request, HttpServletResponse response)throws ServletException, IOException 
     {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
 
-            String[]tab=request.getRequestURL().toString().split("/");
+            String[]tab=request.getRequestURL().toString().split("Construct-FrameWork");
             String result=tab[tab.length-1];
 
             /* TODO output your page here. You may use following sample code. */
@@ -24,7 +43,7 @@ public class FrontServlet extends HttpServlet {
             out.println("<title>Servlet ListServiceController</title>");            
             out.println("</head>");
             out.println("<body>");
-            out.println("<h1>" + result + "</h1>");
+            out.println("<h1>" + getmappingUrls().size() + "</h1>");
             out.println("</body>");
             out.println("</html>");
         }
